aa590b506c3ac7b597511f0eaa5db0327e930e4b
HenrikAnderss0n <236362501+HenrikAnderss0n@users.noreply.github.com>
2026-04-20 15:40:08 +0200

    run_analysis script

---
+++ run_analysis.R
@@ -0,0 +1,7 @@
+library(here)
+library(tidyverse)
+
+source(here("R", "01_dataforstaelse.R"))
+source(here("R", "02_datastadning_och_forberedelse.R"))
+source(here("R", "03_statistiska_sammanfattningar.R"))
+source(here("R", "04_visualisering.R"))
