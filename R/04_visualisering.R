4d54d5239a3d8807f0563ef35582202c75695872
Azar Mohseni <azar_mohseni@icloud.com>
2026-04-21 15:08:02 +0200

    Update source file paths for data scripts

--- R/04_visualisering.R
+++ R/04_visualisering.R
@@ -1,7 +1,6 @@
 library(tidyverse)
-library(here)
-source(here("R","01_dataforstaelse.R"))
-source(here("R", "02_datastadning_och_forberedelse.R"))
+source("R/01_dataforstaelse.R")
+source("R/02_datastadning_och_forberedelse.R")
 
 # FIGUR 1 – Frågeställning 1: Vilka produktkategorier driver högst försäljning?
 
